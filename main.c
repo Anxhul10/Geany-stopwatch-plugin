31b92bf3397d04933e98ca83c1050b8cdb701dde
Anshul Ekka <anxhul10@gmail.com>
2026-04-10 23:57:23 +0530

    chore: change metadata

--- main.c
+++ main.c
@@ -1,24 +1,46 @@
 #include <geanyplugin.h>
-
-static gboolean hello_init(GeanyPlugin *plugin, gpointer pdata) {
-    g_print("Hello from Geany Plugin!\n");
+ 
+static void item_activate_cb(GtkMenuItem *menuitem, gpointer user_data)
+{
+    dialogs_show_msgbox(GTK_MESSAGE_INFO, "Hello World");
+}
+ 
+ 
+static gboolean hello_init(GeanyPlugin *plugin, gpointer pdata)
+{
+    GtkWidget *main_menu_item;
+ 
+    // Create a new menu item and show it
+    main_menu_item = gtk_menu_item_new_with_mnemonic("Hello World");
+    gtk_widget_show(main_menu_item);
+    gtk_container_add(GTK_CONTAINER(plugin->geany_data->main_widgets->tools_menu),
+            main_menu_item);
+    g_signal_connect(main_menu_item, "activate",
+            G_CALLBACK(item_activate_cb), NULL);
+ 
+    geany_plugin_set_data(plugin, main_menu_item, NULL);
     return TRUE;
 }
-
-static void hello_cleanup(GeanyPlugin *plugin, gpointer pdata) {
-    g_print("Goodbye!\n");
+ 
+ 
+static void hello_cleanup(GeanyPlugin *plugin, gpointer pdata)
+{
+    GtkWidget *main_menu_item = (GtkWidget *) pdata;
+ 
+    gtk_widget_destroy(main_menu_item);
 }
-
+ 
+ 
 G_MODULE_EXPORT
-void geany_load_module(GeanyPlugin *plugin) {
+void geany_load_module(GeanyPlugin *plugin)
+{
     plugin->info->name = "Stopwatch";
     plugin->info->description = "A watch which can be started and stopped by pressing a button, so that you can measure exactly how long something takes";
     plugin->info->version = "0.0.1";
     plugin->info->author = "Anshul Ekka <anxhul10@gmail.com>";
-
-    // init and cleanup are mandatory
+ 
     plugin->funcs->init = hello_init;
     plugin->funcs->cleanup = hello_cleanup;
-
+ 
     GEANY_PLUGIN_REGISTER(plugin, 225);
 }
